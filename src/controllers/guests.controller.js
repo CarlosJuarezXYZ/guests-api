2305f4316fa03f2360de3d1b35af8883547051bc
CarlosJuarezXYZ <carlostuna015@gmail.com>
2025-03-12 18:42:08 -0500

    feat(CHARLS): add create update and delete controllers and route

--- src/controllers/guests.controller.js
+++ src/controllers/guests.controller.js
@@ -8,3 +8,61 @@ export const getGuests = async (req, res) => {
     return res.status(500).json({ message: "something goes wrong" });
   }
 };
+
+export const createGuests = async (req, res) => {
+  try {
+    const { name, email, phone, age, relationship, attending } = req.body;
+    const [rows] = await pool.query(
+      "INSERT INTO guests (name,email,phone,age,relationship,attending) VALUES (?,?,?,?,?,?)",
+      [name, email, phone, age, relationship, attending]
+    );
+    res
+      .status(201)
+      .json({
+        id: rows.insertId,
+        name,
+        email,
+        phone,
+        age,
+        relationship,
+        attending,
+      });
+  } catch (error) {
+    console.log(error,error.message,'error');
+    return res.status(500).json({ message: "something goes wrong" });
+  }
+};
+
+export const updateGuests = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, email, phone, age, relationship, attending } = req.body;
+
+    const [result] = await pool.query(
+      "UPDATE guests set name = IFNULL(?,name), email = IFNULL(?,email), phone = IFNULL(?,phone), age = IFNULL(?,age), relationship = IFNULL(?,relationship), attending = IFNULL(?,attending) WHERE id = ?",
+      [name, email, phone, age, relationship, attending, id]
+    );
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: "Guest not found" });
+    }
+
+    const [rows] = await pool.query("SELECT * FROM guests WHERE id = ?", [id]);
+    res.json(rows[0]);
+  } catch (error) {
+    return res.status(500).json({ message: "something goes wrong" });
+  }
+};
+
+export const deleteGuests = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [rows] = await pool.query("DELETE FROM guests WHERE id = ?", [id]);
+
+    if (rows.affectedRows <= 0) {
+      return res.status(404).json({ message: "Guest not found" });
+    }
+    res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ message: "Something goes wrong" });
+  }
+};
--- src/routes/guests.routes.js
+++ src/routes/guests.routes.js
@@ -1,8 +1,14 @@
 import {Router} from 'express';
-import { getGuests } from '../controllers/guests.controller.js';
+import { getGuests,createGuests, updateGuests, deleteGuests } from '../controllers/guests.controller.js';
 
 const router = Router();
 
 router.get("/guests", getGuests);
+
+router.post("/guests", createGuests);
+
+router.patch("/guests/:id",updateGuests);
+
+router.delete("/guests/:id",deleteGuests);
 
 export default router;
