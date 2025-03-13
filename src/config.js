6e3a806bd9de16ce0e801ccfd1185d753d653299
CarlosJuarezXYZ <carlostuna015@gmail.com>
2025-03-13 11:39:54 -0500

    feat(CHARLS): add console.log of variables to connection database

--- src/config.js
+++ src/config.js
@@ -1,6 +1,12 @@
 import {config} from 'dotenv';
 config();
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_DATABASE:", process.env.DB_DATABASE);
+console.log("DB_PORT:", process.env.DB_PORT);
+
 export const PORT = process.env.PORT || 3000;
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_USER = process.env.DB_USER || 'root';
